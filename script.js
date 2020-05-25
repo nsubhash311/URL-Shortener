94fa2b997e47b49171b1c6a501fbe28c2427b72e
Subhash <nsubhash311@gmail.com>
2020-05-26 01:44:18 +0530

    final commit

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-    var url=document.getElementById("getlink")
+var url=document.getElementById("getlink")
+var link = document.querySelector('.link')
 function myFunction() 
 {
-    fetch(`https://shorturl-sfy-cx.p.rapidapi.com/?url=${url.value}`, 
-    {
-    "method": "GET",
-    "headers": 
-    {
-        "x-rapidapi-host": "shorturl-sfy-cx.p.rapidapi.com",
-        "x-rapidapi-key": "b10a154f03msh0bc6f60bcdc6180p15afb1jsn14ea480096bf"
+axios({
+    "method":"GET",
+    "url":"https://shorturl-sfy-cx.p.rapidapi.com/",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-rapidapi-host":"shorturl-sfy-cx.p.rapidapi.com",
+    "x-rapidapi-key":"b10a154f03msh0bc6f60bcdc6180p15afb1jsn14ea480096bf",
+    "useQueryString":true
+    },"params":{
+    "url":url.value
     }
     })
-.then(response => 
-    {
-    console.log(response);
+    .then((response)=>{
+      const shortlink = response.data
+      link.innerHTML=shortlink
+      const shortenedlink = document.querySelector('a')
+      shortenedlink.setAttribute('target', '_blank');
+      url.value = shortenedlink.innerText      
     })
-.catch(err => 
-    {
-    console.log(err);
-    });
+    .catch((error)=>{
+      console.log(error)
+    })
 }
-		
+
+function copyText(){
+  url.select();;
+  document.execCommand("copy");
+}
